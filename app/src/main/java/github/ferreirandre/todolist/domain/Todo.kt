69b735b2fa3dde27e0f7d7b9d1e5c1d00b99a30c
FerreirAndre <adrecruz15@gmail.com>
2026-09-02 16:19:33 -0300

    chore: create todo data class

---
+++ app/src/main/java/github/ferreirandre/todolist/domain/Todo.kt
@@ -0,0 +1,31 @@
+package github.ferreirandre.todolist.domain
+
+data class Todo(
+    val id: Long,
+    val title: String,
+    val description: String?,
+    val isCompleted: Boolean,
+)
+
+// fake objects
+
+val todo1 = Todo(
+    id = 1,
+    title = "primeira tarefa",
+    description = "descricao da tarefa 1",
+    isCompleted = false
+)
+
+val todo2 = Todo(
+    id = 2,
+    title = "segunda tarefa",
+    description = "descricao da tarefa 2",
+    isCompleted = false
+)
+
+val todo3 = Todo(
+    id = 3,
+    title = "terceira tarefa",
+    description = "descricao da tarefa 3",
+    isCompleted = false
+)
